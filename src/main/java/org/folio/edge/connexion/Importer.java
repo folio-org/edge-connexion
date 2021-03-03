67ba59ffe3021d05c475dde86b94607dabc30656
Adam Dickmeiss <adam@indexdata.dk>
2021-03-03 13:04:40 +0100

    Parse records (#3)
    Add FOLIO checkstyle.

---
+++ src/main/java/org/folio/edge/connexion/Importer.java
@@ -0,0 +1,102 @@
+package org.folio.edge.connexion;
+
+import io.vertx.core.Future;
+import io.vertx.core.buffer.Buffer;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Importer {
+  private static final Logger log = LogManager.getLogger(Importer.class);
+
+  private final Buffer user = Buffer.buffer();
+  private final Buffer localUser = Buffer.buffer();
+  private final Buffer password = Buffer.buffer();
+  private final List<Buffer> records = new LinkedList<>();
+
+  private static int parseValue(Buffer buffer, int i, Buffer v) {
+    // two length specs
+    //  1. single character specifies, where @=0, A=1, B=2, ... DEL=63 (only up to 63 in length !!)
+    //  2. multiple digits, followed by value (a value may not start with a digit!!)
+    i++;
+    int len = buffer.getByte(i) - 64;
+    if (len >= 0) {
+      i++; // 1: single character length, skip length itself
+    } else {
+      // 2: digits makes up a number.
+      int llen = 0;
+      while (buffer.getByte(i + llen) >= 48 && buffer.getByte(i + llen) < 58) {
+        llen++;
+      }
+      len = Integer.parseInt(buffer.getString(i, i + llen));
+      i += llen;
+    }
+    v.appendBuffer(buffer, i, len);
+    return i + len;
+  }
+
+  private static int parseMarc(Buffer buffer, int i, List<Buffer> records) {
+    int len = Integer.parseInt(buffer.getString(i, i + 5));
+    records.add(buffer.slice(i, i + len));
+    return i + len;
+  }
+
+  String getUser() {
+    return user.toString();
+  }
+
+  String getLocalUser() {
+    return localUser.toString();
+  }
+
+  String getPassword() {
+    return password.toString();
+  }
+
+  List<Buffer> getRecords() {
+    return records;
+  }
+
+  /**
+   * Parse request from OCLC Connexion client.
+   * May throw exception for bad buffer.
+   * @param buffer OCOC Connexion buffer.
+   */
+  void parseRequest(Buffer buffer) {
+    int i = 0;
+    while (i < buffer.length()) {
+      switch (buffer.getByte(i)) {
+        case 'U':
+          i = parseValue(buffer, i, user);
+          break;
+        case 'A':
+          i = parseValue(buffer, i, localUser);
+          break;
+        case 'P':
+          i = parseValue(buffer, i, password);
+          break;
+        case ' ':
+          i++;
+          break;
+        default:
+          i = parseMarc(buffer, i, records);
+      }
+    }
+  }
+
+  /**
+   * Parse request from OCLC Connexion client.
+   * @param buffer OCLC Connexion buffer.
+   * @return Future failed future on bad buffer. succeeded future otherwise.
+   */
+  Future<Void> importRequest(Buffer buffer) {
+    try {
+      parseRequest(buffer);
+    } catch (Exception e) {
+      log.warn(e.getMessage(), e);
+      return Future.failedFuture(e);
+    }
+    return Future.succeededFuture();
+  }
+}
--- src/main/java/org/folio/edge/connexion/MainVerticle.java
+++ src/main/java/org/folio/edge/connexion/MainVerticle.java
@@ -5,61 +5,69 @@ import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.folio.edge.core.Constants;
 import org.folio.edge.core.EdgeVerticleCore;
-
-import static org.folio.edge.core.Constants.SYS_PORT;
 
 public class MainVerticle extends EdgeVerticleCore {
 
-    private static final int MAX_RECORD_SIZE = 100000;
-    private static final int DEFAULT_PORT = 8081;
-    private static final Logger log = LogManager.getLogger(MainVerticle.class);
-    private int maxRecordSize = MAX_RECORD_SIZE;
+  private static final int MAX_RECORD_SIZE = 100000;
+  private static final int DEFAULT_PORT = 8081;
+  private static final Logger log = LogManager.getLogger(MainVerticle.class);
+  private int maxRecordSize = MAX_RECORD_SIZE;
+  int port;
 
-    void setMaxRecordSize(int sz) {
-        maxRecordSize = sz;
-    }
+  void setMaxRecordSize(int sz) {
+    maxRecordSize = sz;
+  }
 
-    int getMaxRecordSize() {
-      return maxRecordSize;
-    }
+  int getMaxRecordSize() {
+    return maxRecordSize;
+  }
 
-    int port;
-    @Override
-    public void start(Promise<Void> promise) {
-        Future.<Void>future(p -> super.start(p)).<Void>compose(res -> {
-            port = config().getInteger(SYS_PORT, DEFAULT_PORT);
-            return vertx.createNetServer()
-                    .connectHandler(socket -> {
-                        Buffer buffer = Buffer.buffer();
-                        // handle both HTTP For /admin/health and for the Connexion callback
-                        socket.handler(chunk -> {
-                            buffer.appendBuffer(chunk);
-                            for (int i = 0; i < buffer.length(); i++) {
-                              // look for LFCRLF or LFLF
-                              if (buffer.getByte(i) == '\n') {
-                                int j = i + 1;
-                                if (j < buffer.length() && buffer.getByte(j) == '\r') {
-                                  j++;
-                                }
-                                if (j < buffer.length() && buffer.getByte(j) == '\n') {
-                                  if ("GET ".equals(buffer.getString(0, 4))) {
-                                    // close now, ignoring keep alive (which is our right!)
-                                    log.debug("Got HTTP: {}", buffer.toString());
-                                    socket.write("HTTP/1.0 200 OK\r\n\r\n")
-                                        .onComplete(x -> socket.close());
-                                  }
-                                }
-                              }
-                            }
-                            if (buffer.length() > maxRecordSize) {
-                                socket.close();
-                            }
-                        });
-                        socket.endHandler(end -> {
-                            log.info("Got buffer of size {}", buffer.length());
-                        });
-                    }).listen(port).mapEmpty();
-        }).onComplete(promise);
-    }
+  @Override
+  public void start(Promise<Void> promise) {
+    Future.<Void>future(p -> super.start(p)).<Void>compose(res -> {
+      port = config().getInteger(Constants.SYS_PORT, DEFAULT_PORT);
+      return vertx.createNetServer()
+          .connectHandler(socket -> {
+            // handle both HTTP For /admin/health and request Connexion client
+            Buffer buffer = Buffer.buffer();
+            socket.handler(chunk -> {
+              buffer.appendBuffer(chunk);
+              log.info("handler size {} , max {}", buffer.length(), maxRecordSize);
+              if (buffer.length() > maxRecordSize) {
+                log.warn("OCLC import size exceeded {}", maxRecordSize);
+                socket.endHandler(x -> {});
+                socket.close();
+                return;
+              }
+              for (int i = 0; i < buffer.length(); i++) {
+                // look for LFCRLF or LFLF
+                if (buffer.getByte(i) == '\n') {
+                  int j = i + 1;
+                  if (j < buffer.length() && buffer.getByte(j) == '\r') {
+                    j++;
+                  }
+                  if (j < buffer.length() && buffer.getByte(j) == '\n') {
+                    if ("GET ".equals(buffer.getString(0, 4))) {
+                      // close now, ignoring keep alive (which is our right!)
+                      socket.endHandler(x -> {});
+                      log.debug("Got HTTP: {}", buffer.toString());
+                      socket.write("HTTP/1.0 200 OK\r\n\r\n")
+                          .onComplete(x -> socket.close());
+                      return;
+                    }
+                  }
+                }
+              }
+            });
+            socket.endHandler(end -> {
+              Importer importer = new Importer();
+              importer.importRequest(buffer)
+                  .onFailure(cause -> log.warn(cause.getMessage(), cause))
+                  .onSuccess(complete -> log.info("Importing successfully"));
+            });
+          }).listen(port).mapEmpty();
+    }).onComplete(promise);
+  }
 }
