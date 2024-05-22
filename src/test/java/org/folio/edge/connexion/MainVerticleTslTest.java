7a6071fbd761b253b378977610fe1a3f21700b4f
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-05-22 19:42:00 +0500

    added integration test

--- src/test/java/org/folio/edge/connexion/MainVerticleTslTest.java
+++ src/test/java/org/folio/edge/connexion/MainVerticleTslTest.java
@@ -1,34 +1,44 @@
 package org.folio.edge.connexion;
 
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.json.JsonObject;
-import io.vertx.core.net.KeyStoreOptions;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.WebClientOptions;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
-import org.folio.edge.core.Constants;
-import org.folio.edge.core.utils.ApiKeyUtils;
-import org.folio.edge.core.utils.SslConfigurationUtil;
-import org.folio.edge.core.utils.test.MockOkapi;
-import org.folio.edge.core.utils.test.TestUtils;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.spy;
 
 import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.spy;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import org.folio.edge.core.Constants;
+import org.folio.edge.core.utils.ApiKeyUtils;
+import org.folio.edge.core.utils.OkapiClient;
+import org.folio.edge.core.utils.OkapiClientFactory;
+import org.folio.edge.core.utils.OkapiClientFactoryInitializer;
+import org.folio.edge.core.utils.SslConfigurationUtil;
+import org.folio.edge.core.utils.test.MockOkapi;
+import org.folio.edge.core.utils.test.TestUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 @RunWith(VertxUnitRunner.class)
 public class MainVerticleTslTest {
@@ -36,26 +46,28 @@ public class MainVerticleTslTest {
   private static final Logger logger = LogManager.getLogger(MainVerticleTest.class);
   static Vertx vertx;
 
-  static final String KEYSTORE_TYPE = "BCFKS";
-  static final String KEYSTORE_PATH = "test.keystore 1.bcfks";
-  static final String TRUST_STORE_PATH = "test.truststore 1.bcfks";
-  static final String KEYSTORE_PASSWORD = "SecretPassword";
-  static final String RESPONSE_MESSAGE = "<OK>";
+  private static final String KEYSTORE_TYPE = "BCFKS";
+  private static final String KEYSTORE_PATH = "test.keystore 1.bcfks";
+  private static final String TRUST_STORE_PATH = "test.truststore 1.bcfks";
+  private static final String KEYSTORE_PASSWORD = "SecretPassword";
+  private static final String RESPONSE_MESSAGE = "<OK>";
+  private static final String OKAPI_URL = "http://localhost:" + TestUtils.getPort();
+  private static final String TENANT = "diku";
 
-  @BeforeClass
-  public static void setUpOnce(TestContext context) {
+  @Before
+  public void setUpOnce() {
     Security.addProvider(new BouncyCastleFipsProvider());
     vertx = Vertx.vertx();
   }
 
-  @AfterClass
-  public static void tearDownOnce(TestContext context) {
+  @After
+  public void tearDown(TestContext context) {
     vertx.close(context.asyncAssertSuccess());
   }
 
   @Test
-  public void testServerClientTlsCommunication(TestContext context) {
-    final JsonObject config = getCommonConfig();
+  public void testServerClientTlsCommunication(TestContext context) throws IllegalAccessException {
+    final JsonObject config = getCommonConfig(true);
 
     final HttpServerOptions serverOptions = new HttpServerOptions();
     serverOptions.setPort(config.getInteger(Constants.SYS_PORT));
@@ -64,19 +76,78 @@ public class MainVerticleTslTest {
 
     final HttpServer httpServer = vertx.createHttpServer(serverOptions);
     httpServer
-        .requestHandler(req -> req.response().putHeader("content-type", "text/plain").end(RESPONSE_MESSAGE))
-        .listen(config.getInteger(Constants.SYS_PORT), http -> logger.info("Server started on port {}", config.getInteger(Constants.SYS_PORT)));
+        .requestHandler(req -> req.response().putHeader(HttpHeaders.CONTENT_TYPE, Constants.TEXT_PLAIN).end(RESPONSE_MESSAGE))
+        .listen(config.getInteger(Constants.SYS_PORT), getCommonServerHandler(config));
 
-    final WebClientOptions clientOptions = new WebClientOptions();
-    clientOptions
-        .setSsl(config.getBoolean(Constants.SYS_WEB_CLIENT_SSL_ENABLED))
-        .setVerifyHost(true)
-        .setTrustOptions(new KeyStoreOptions()
-            .setType(config.getString(Constants.SYS_WEB_CLIENT_TRUSTSTORE_TYPE))
-            .setPath(config.getString(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PATH))
-            .setPassword(config.getString(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PASSWORD)));
-    final WebClient webClient = WebClient.create(vertx, clientOptions);
+    final OkapiClientFactory okapiClientFactory = OkapiClientFactoryInitializer.createInstance(vertx, config);
+    final OkapiClient okapiClient = okapiClientFactory.getOkapiClient(TENANT);
+    final WebClientOptions webClientOptions = (WebClientOptions) FieldUtils.readDeclaredField(okapiClient.client, "options", true);
 
+    assertTrue(webClientOptions.isSsl());
+    assertNotNull(webClientOptions.getTrustOptions());
+
+    createClientRequest(context, webClientOptions, config);
+  }
+
+  @Test(expected = java.net.ConnectException.class)
+  public void testFailingServerClientTlsCommunication(TestContext context) throws IllegalAccessException {
+    final JsonObject config = getCommonConfig(false);
+
+    final HttpServerOptions serverOptions = new HttpServerOptions();
+    serverOptions.setPort(config.getInteger(Constants.SYS_PORT));
+
+    SslConfigurationUtil.configureSslServerOptionsIfEnabled(config, serverOptions);
+
+    final HttpServer httpServer = vertx.createHttpServer(serverOptions);
+    httpServer
+        .requestHandler(req -> req.response().putHeader(HttpHeaders.CONTENT_TYPE, Constants.TEXT_PLAIN).end(RESPONSE_MESSAGE))
+        .listen(config.getInteger(Constants.SYS_PORT), getCommonServerHandler(config));
+
+    final OkapiClientFactory okapiClientFactory = OkapiClientFactoryInitializer.createInstance(vertx, config);
+    final OkapiClient okapiClient = okapiClientFactory.getOkapiClient(TENANT);
+    final WebClientOptions webClientOptions = (WebClientOptions) FieldUtils.readDeclaredField(okapiClient.client, "options", true);
+
+    assertFalse(webClientOptions.isSsl());
+    assertNull(webClientOptions.getTrustOptions());
+
+    createClientRequest(context, webClientOptions, config);
+  }
+
+  private static Handler<AsyncResult<HttpServer>> getCommonServerHandler(JsonObject config) {
+    return http -> logger.info("Server started on port {}", config.getInteger(Constants.SYS_PORT));
+  }
+
+  @Test
+  public void testSetupCorrectTslConfig(TestContext context) throws Exception {
+    JsonObject config = getCommonConfig(false);
+    deployVerticle(context, config);
+  }
+
+  private JsonObject getCommonConfig(boolean enableWebClientSsl) {
+    int serverPort = TestUtils.getPort();
+    JsonObject config = new JsonObject().put(Constants.SYS_PORT, serverPort)
+        .put(Constants.SYS_OKAPI_URL, OKAPI_URL)
+        .put(Constants.SYS_SECURE_STORE_PROP_FILE, "src/test/resources/ephemeral.properties")
+        .put(Constants.SYS_LOG_LEVEL, "TRACE")
+        .put(Constants.SYS_REQUEST_TIMEOUT_MS, 5000)
+        .put(Constants.SYS_HTTP_SERVER_SSL_ENABLED, true)
+        .put(Constants.SYS_HTTP_SERVER_KEYSTORE_TYPE, KEYSTORE_TYPE)
+        .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PATH, KEYSTORE_PATH)
+        .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PASSWORD, KEYSTORE_PASSWORD);
+    if (enableWebClientSsl) {
+      return config
+          .put(Constants.SYS_WEB_CLIENT_SSL_ENABLED, true)
+          .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_TYPE, KEYSTORE_TYPE)
+          .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PATH, TRUST_STORE_PATH)
+          .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PASSWORD, KEYSTORE_PASSWORD);
+    } else {
+      return config
+          .put(Constants.SYS_WEB_CLIENT_SSL_ENABLED, false);
+    }
+  }
+
+  private static void createClientRequest(TestContext context, WebClientOptions webClientOptions, JsonObject config) {
+    final WebClient webClient = WebClient.create(vertx, webClientOptions);
     webClient.get(config.getInteger(Constants.SYS_PORT), "localhost", "/")
         .send()
         .onComplete(context.asyncAssertSuccess(response -> {
@@ -85,25 +156,6 @@ public class MainVerticleTslTest {
           context.assertEquals(HttpResponseStatus.OK.code(), response.statusCode());
           context.assertEquals(RESPONSE_MESSAGE, message);
         }));
-  }
-
-  @Test
-  public void testSetupCorrectTslConfig(TestContext context) throws Exception {
-    JsonObject config = getCommonConfig();
-    deployVerticle(context, config);
-  }
-
-  private JsonObject getCommonConfig() {
-    int serverPort = TestUtils.getPort();
-    return new JsonObject().put(Constants.SYS_PORT, serverPort)
-        .put(Constants.SYS_HTTP_SERVER_SSL_ENABLED, true)
-        .put(Constants.SYS_HTTP_SERVER_KEYSTORE_TYPE, KEYSTORE_TYPE)
-        .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PATH, KEYSTORE_PATH)
-        .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PASSWORD, KEYSTORE_PASSWORD)
-        .put(Constants.SYS_WEB_CLIENT_SSL_ENABLED, true)
-        .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_TYPE, KEYSTORE_TYPE)
-        .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PATH, TRUST_STORE_PATH)
-        .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PASSWORD, KEYSTORE_PASSWORD);
   }
 
   private void deployVerticle(TestContext context, JsonObject config) throws ApiKeyUtils.MalformedApiKeyException {
